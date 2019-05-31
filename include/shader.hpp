c65f47a543a97364187b4920d3882fe9e72ad33d
Patrick Haehn <haehnpt@gmail.com>
2019-05-31 15:55:28 +0200

    Add basic framework

---
+++ include/shader.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "common.hpp"
+
+const char*
+loadShaderFile(const char* filename);
+
+// loads a shader source file, tries to compile the shader
+// and checks for compilation errors
+unsigned int
+compileShader(const char* filename, unsigned int type);
+
+unsigned int
+linkProgram(unsigned int vertexShader, unsigned int fragmentShader);
