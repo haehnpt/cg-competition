b789b66d0b7a8a770dd184ebad3cb3e76886dc6f
Patrick Haehn <haehnpt@gmail.com>
2019-07-03 00:01:33 +0200

    Clean up code

--- src/terraining_testing.cpp
+++ src/terraining_testing.cpp
@@ -96,18 +96,8 @@ main(int, char* argv[]) {
 
 	// "ffmpeg" command and preparation
 	ffmpeg_wrapper fw(WINDOW_WIDTH, WINDOW_HEIGHT, FRAMES * 3);
-	/*
-	int width = WINDOW_WIDTH;// -100;
-	int height = WINDOW_HEIGHT;// -100;
-	std::string cmd = std::string("\"") + FFMPEG_ROOT + std::string("\\ffmpeg.exe\" -r 60 -f rawvideo -pix_fmt rgba -s " + std::to_string(width) + "x" + std::to_string(height) + " -i - "
-		"-threads 0 -preset fast -y -pix_fmt yuv420p -crf 21 -vf vflip ") + FFMPEG_ROOT + std::string("\\Terrain_Rising.mp4");// +std::string("\"") + FFMPEG_ROOT + std::string("\\Animation_PH_SK_VS.mp4\"");
-	FILE
-	* ffmpeg = _popen(cmd.c_str(), "wb");
-	*/
 
     // rendering loop
-	int frames = 1200;
-	int frame = 0;
 	while (glfwWindowShouldClose(window) == false)
 	{
 		glfwPollEvents();
@@ -134,21 +124,11 @@ main(int, char* argv[]) {
 		// Render terrain
 		terr.render(model_mat_loc);
 
-		// Light motion (DO NOT)
-		//light_phi = (light_phi += 0.01) > 2 * M_PI ? 0.0 : light_phi;
-
 		// Rotate camera
 		cam.rotate();
 
 		// Before swapping, read the pixels and feed them to "ffmpeg"
 		fw.save_frame();
-		/*
-		int * buffer = new int[width * height];
-		glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-		fwrite(buffer, sizeof(int)*width*height, 1, ffmpeg);
-		delete[] buffer;
-		frame++;
-		*/
 
         // render UI
         glfwSwapBuffers(window);
