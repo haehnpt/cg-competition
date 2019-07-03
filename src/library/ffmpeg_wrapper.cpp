ea4610d4720bd891458f279b3787d475856e53bd
Volker Sobek <reklov@live.com>
2019-07-03 02:24:37 +0200

    ffmpeg_wrapper: Support Linux

--- src/library/ffmpeg_wrapper.cpp
+++ src/library/ffmpeg_wrapper.cpp
@@ -6,14 +6,30 @@ ffmpeg_wrapper::ffmpeg_wrapper(int width, int height, int frames)
 	this->height = height;
 	this->frames = frames;
 	this->frame_counter = 0;
+#ifdef __linux__
+	std::string cmd = "ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s "
+	  + std::to_string(width) + "x" + std::to_string(height) + " -i - "
+	  + "-threads 0 -preset fast -y -pix_fmt yuv420p -crf 21 -vf vflip "
+	  + FFMPEG_ROOT + "Terrain_Rising.mp4";
+        ffmpeg = popen(cmd.c_str(), "w");
+#else
 	std::string cmd = std::string("\"") + FFMPEG_ROOT + std::string("\\ffmpeg.exe\" -r 60 -f rawvideo -pix_fmt rgba -s " + std::to_string(width) + "x" + std::to_string(height) + " -i - "
 		"-threads 0 -preset fast -y -pix_fmt yuv420p -crf 21 -vf vflip ") + FFMPEG_ROOT + std::string("\\Terrain_Rising.mp4");
 	ffmpeg = _popen(cmd.c_str(), "wb");
+#endif	// __linux__
+
+	if (!ffmpeg) {
+	  std::cerr << "Error starting ffmpeg process!\n";
+	}
 }
 
 ffmpeg_wrapper::~ffmpeg_wrapper()
 {
+#ifdef __linux__
+	pclose(ffmpeg);
+#else
 	_pclose(ffmpeg);
+#endif	// __linux__
 }
 
 void ffmpeg_wrapper::save_frame()
