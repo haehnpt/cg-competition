2c8c2f4a672858820aa878aa235f277badbd4ea2
Patrick Haehn <haehnpt@gmail.com>
2019-07-10 20:32:31 +0200

    Comment ffmpeg class

--- include/ffmpeg_wrapper.hpp
+++ include/ffmpeg_wrapper.hpp
@@ -4,21 +4,36 @@
 #include "config.hpp"
 #include "common.hpp"
 
+/*
+
+Wrapper class for the GNU General Public Licensed Tool FFMPEG
+which is used in this project to render a 1920x1080@60FPS
+video 
+
+@author Patrick Hähn
+
+ */
 class ffmpeg_wrapper
 {
-    // Private member properties
+    // Rendered video width
 	int width;
+	// Rendered video height
 	int height;
+	// Number of frames to be rendered
 	int frames;
+	// Frame counter
 	int frame_counter;
+	// Handle for the video file to be created
 	FILE * ffmpeg;
 
 public:
-    // Public constructor & destructor
+    // Create a new ffmpeg_wrapper instance
 	ffmpeg_wrapper(int width, int height, int frames, char * render_filename);
+	// Clear up
 	~ffmpeg_wrapper();
 
-    // Public member functions
+    // Save a frame, e.g. send it to FFMPEG
 	void save_frame();
+	// Retrieve whether the rendering has finished
 	bool is_finished();
 };
--- src/library/ffmpeg_wrapper.cpp
+++ src/library/ffmpeg_wrapper.cpp
@@ -1,47 +1,65 @@
 #include "ffmpeg_wrapper.hpp"
 
+// Create a new ffmpeg_wrapper instance
 ffmpeg_wrapper::ffmpeg_wrapper(int width, int height, int frames, char * render_filename)
 {
+	// Set properties
 	this->width = width;
 	this->height = height;
 	this->frames = frames;
 	this->frame_counter = 0;
-#ifdef __linux__
+
+	// Distinguish between linux and windows
+	// and call ffmpeg accordingly
+    #ifdef __linux__
 	std::string cmd = "ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s "
 	  + std::to_string(width) + "x" + std::to_string(height) + " -i - "
 	  + "-threads 0 -preset fast -y -pix_fmt yuv420p -crf 21 -vf vflip "
 	  + FFMPEG_ROOT + std::string(render_filename);
         ffmpeg = popen(cmd.c_str(), "w");
-#else
+    #else
 	std::string cmd = std::string("\"") + FFMPEG_ROOT + std::string("ffmpeg.exe\" -r 60 -f rawvideo -pix_fmt rgba -s " + std::to_string(width) + "x" + std::to_string(height) + " -i - "
 		"-threads 0 -preset fast -y -pix_fmt yuv420p -crf 15 -vf vflip ") + FFMPEG_ROOT + std::string(render_filename);
 	ffmpeg = _popen(cmd.c_str(), "wb");
-#endif	// __linux__
+    #endif
 
+    // Check if the handle has been created
 	if (!ffmpeg) {
 	  std::cerr << "Error starting ffmpeg process!\n";
 	}
 }
 
+// Clear up
 ffmpeg_wrapper::~ffmpeg_wrapper()
 {
-#ifdef __linux__
+	// Distinguish between linux and windows
+	// and close the file handle accordingly
+    #ifdef __linux__
 	pclose(ffmpeg);
-#else
+    #else
 	_pclose(ffmpeg);
-#endif	// __linux__
+    #endif
 }
 
+// Save a frame, e.g. send it to FFMPEG
 void ffmpeg_wrapper::save_frame()
 {
+    // Create a local buffer of the necessary size
 	int * buffer = new int[width * height];
+
+    // Read the frame buffer and write it to the ffmpeg handle
 	glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
 	fwrite(buffer, sizeof(int)*width*height, 1, ffmpeg);
+
+    // Delete the local buffer and increase the frame counter 
 	delete[] buffer;
 	++frame_counter;
 }
 
+// Retrieve whether the rendering has finished
 bool ffmpeg_wrapper::is_finished()
 {
+    // The rendering has finished when the frame counter
+	// is greater than the maximum number of frames
 	return frame_counter > frames;
 }
